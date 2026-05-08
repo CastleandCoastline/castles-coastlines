4c077644606323db2dfbb9cec2f4ecf37893f29d
Admin <admin@Users-MacBook-Pro.local>
2026-05-08 11:33:19 +0100

    Fix stats page routing in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import MenuOrder from './MenuOrder';
+import StatsPage from './StatsPage';
 
 const path = window.location.pathname;
-const Component = path.startsWith('/menu') ? MenuOrder : App;
+const Component = path.startsWith('/menu') ? MenuOrder : path.startsWith('/stats') ? StatsPage : App;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<React.StrictMode><Component /></React.StrictMode>);

24c5c291ee427d4e6c0a75b547c5c66dca1d85a7
Admin <admin@Users-MacBook-Pro.local>
2026-05-08 12:31:28 +0100

    Add Capacitor iOS project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.uk.castleandcoastline',
+  appName: 'Castle and Coastline',
+  webDir: 'build'
+};
+
+export default config;
